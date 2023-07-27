eb56be6dab5293910ba67a093f382888613d2405
guoxq971 <1498820625@qq.com>
2023-07-27 14:13:42 +0800

    feat: vue3 的qiankun配置(未完成)

--- sub-vue3-three-design/src/main.ts
+++ sub-vue3-three-design/src/main.ts
@@ -3,17 +3,91 @@ import './assets/main.css';
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import router from './router';
+// import router from './router';
+import { routes } from './router';
 
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import { store } from '@/stores/index.ts';
+import { createRouter, createWebHistory } from 'vue-router';
 
 // @ts-ignore
-const app = createApp(App);
+// const app = createApp(App);
+//
+// app.use(store);
+// app.use(Antd);
+// app.use(router);
+//
+// app.mount('#app');
 
-app.use(store);
-app.use(Antd);
-app.use(router);
+let router = null;
+let instance = null;
+let state = null;
 
-app.mount('#app');
+function render(props = {}) {
+  const { container } = props;
+  // router = new VueRouter({
+  //   base: window.__POWERED_BY_QIANKUN__ ? "/vue" : "/",
+  //   mode: "history",
+  //   routes,
+  // });
+
+  const router = createRouter({
+    base: window.__POWERED_BY_QIANKUN__ ? '/vue3' : '/',
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes,
+  });
+
+  // instance = new Vue({
+  //   router,
+  //   store,
+  //   render: (h) => h(App),
+  // }).$mount(container ? container.querySelector("#app") : "#app");
+
+  const app = createApp(App);
+
+  app.use(store);
+  app.use(Antd);
+  app.use(router);
+
+  app.mount(container ? container.querySelector('#app') : '#app');
+
+  instance = app;
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+// 全局共享状态
+function storeTest(props) {
+  // 监听
+  if (props.onGlobalStateChange) {
+    props.onGlobalStateChange((value, prev) => {
+      if (!state) state = value;
+      console.log(props.name, '监听全局状态', value, prev);
+    }, true);
+  }
+  // 设置
+  if (props.setGlobalState) {
+    // props.setGlobalState({ count: 2 });
+  }
+}
+
+export async function bootstrap(props) {
+  console.log('子应用(vue) 初始化(bootstrap)');
+}
+
+export async function mount(props) {
+  console.log('子应用(vue) 挂载(mount) props=>', props);
+  storeTest(props);
+  render(props);
+}
+
+export async function unmount() {
+  console.log('这是vue1 执行了 unmount');
+  instance.$destroy();
+  instance.$el.innerHTML = '';
+  instance = null;
+  router = null;
+}
--- sub-vue3-three-design/src/router/index.ts
+++ sub-vue3-three-design/src/router/index.ts
@@ -1,14 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-});
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'about',
+//       component: () => import('../views/AboutView.vue'),
+//     },
+//   ],
+// });
 
-export default router;
+export const routes = [
+  {
+    path: '/',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
+  },
+];
+
+// export default router;
